530b2d765fde3fc83827bf265dc4b3b008e19f14
chung2012 <chungyl2012@gmail.com>
2022-11-10 23:30:27 +0800

    war

--- src/main/java/com/github/cylyl/FlutterWebRunner/FlutterWebRunnerApplication.java
+++ src/main/java/com/github/cylyl/FlutterWebRunner/FlutterWebRunnerApplication.java
@@ -2,12 +2,8 @@ package com.github.cylyl.FlutterWebRunner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlutterWebRunnerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FlutterWebRunnerApplication.class, args);
-	}
-
+public class FlutterWebRunnerApplication extends SpringBootServletInitializer {
 }
